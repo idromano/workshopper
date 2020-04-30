a7b57e7830daaaae94c466bbaddd8afb58dff063
Ícaro Romano <37457217+idamaceno@users.noreply.github.com>
2020-04-30 12:14:16 -0300

    Fatoração do código: separando partes do código em funções mais bem definidas

--- Crawler.py
+++ Crawler.py
@@ -22,32 +22,39 @@ def esperar_tantos_segundos(tempo_em_segundos):
     time.sleep(tempo_em_segundos)
 
 
-def listar_itens_inscritos():
+def pegar_lista_de_itens_inscritos():
     """Cria uma planilha com nome, link e link da foto de cada item inscrito na oficina"""
 
-    meus_itens_inscritos = "https://steamcommunity.com/id/damacenikos/myworkshopfiles/?appid=255710" \
-                           "&browsefilter=mysubscriptions" \
-                           "&sortmethod=alpha" \
-                           "&numperpage=30"
-    abrir_pagina(meus_itens_inscritos)
-    proxima_pagina = True
-
-    with open("Meus itens da oficina.csv", "w", encoding="utf-8", newline="") as planilha:
+    with open("Lista dos meus itens inscritos da Workshop.csv", "w", encoding="utf-8", newline="") as planilha:
         writer = csv.writer(planilha)
         writer.writerow(["IMAGEM", "NOME", "LINK"])
-        while proxima_pagina:
-            itens_inscritos = driver.find_elements_by_class_name("itemContents")
-            for item in itens_inscritos:
-                nome = item.find_element_by_class_name("workshopItemTitle").text
-                link = item.find_element_by_css_selector("div.workshopItemSubscriptionDetails > a").get_attribute(
-                    "href")
-                imagem = item.find_element_by_class_name("workshopItemPreviewImage").get_attribute("src")  # miniatura
-                writer.writerow([imagem, nome, link])
-            esperar_tantos_segundos(1)
-            try:
-                botao_de_proxima_pagina = driver.find_elements_by_class_name("pagebtn")[-1]
-                botao_de_proxima_pagina.click()
-            except NoSuchElementException:
-                proxima_pagina = False
 
-#  Próximo passo: função para abrir cada item da lista, coletar dados e adicionar à planilha
+        proxima_pagina = True
+        while proxima_pagina:
+
+            def copiar_info_de_cada_item():
+                itens_inscritos = driver.find_elements_by_class_name("itemContents")
+                for item in itens_inscritos:
+                    nome = item.find_element_by_class_name("workshopItemTitle").text
+                    link = item.find_element_by_css_selector("div.workshopItemSubscriptionDetails > a").get_attribute(
+                        "href")
+                    imagem = item.find_element_by_class_name("workshopItemPreviewImage").get_attribute("src")
+                    writer.writerow([imagem, nome, link])
+
+            def ir_para_proxima_pagina():
+                try:
+                    esperar_tantos_segundos(1)
+                    botao_de_proxima_pagina = driver.find_elements_by_class_name("pagebtn")[-1]
+                    botao_de_proxima_pagina.click()
+                except NoSuchElementException:
+                    nonlocal proxima_pagina
+                    proxima_pagina = False
+
+            meus_itens_inscritos = "https://steamcommunity.com/id/damacenikos/myworkshopfiles/?appid=255710" \
+                                   "&browsefilter=mysubscriptions" \
+                                   "&sortmethod=alpha" \
+                                   "&numperpage=30"
+            abrir_pagina(meus_itens_inscritos)
+            copiar_info_de_cada_item()
+            ir_para_proxima_pagina()
+
